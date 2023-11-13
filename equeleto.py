a9aea35a3f3dc1ef0c8988e797e349cce3d5cf19
joaquinzx03 <150629627+joaquinzx03@users.noreply.github.com>
2023-11-12 23:56:58 -0300

    Add files via upload

--- equeleto.py
+++ equeleto.py
@@ -3,5 +3,36 @@ import tkinter as tk
 # (cuando instalen la libreria pillow saquenle el "#")
 import requests
 
-url:str = "http://vps-3701198-x.dattaweb.com:4000/"
-def main
+#Recuerden de crear las ventanas DENTRO de sus funciones.
+# recordatorio: raiz(vetana)<frame(pantalla)<labels/buttons/entrys
+
+
+def funcion_mostrar_ventana1():
+    pass
+
+
+def funcion_mostrar_ventana2():
+    pass
+
+
+def funcion_mostrar_ventana3():
+    pass
+
+
+def funcion_mostrar_ventana4():
+    pass
+
+
+def main()->None:
+    
+    url:str = "http://vps-3701198-x.dattaweb.com:4000/"
+
+    funcion_mostrar_ventana1()
+    
+    funcion_mostrar_ventana2()
+    
+    funcion_mostrar_ventana3()
+    
+    funcion_mostrar_ventana4()
+
+main()
