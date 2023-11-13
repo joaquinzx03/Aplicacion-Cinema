a103dc609eabb880d1ee0925c6822e0223b69f09
joaquinzx7 <jo.garcia@fi.uba.com>
2023-11-12 21:13:31 -0300

    Se agrega el esqueleto del proyecto

---
+++ equeleto.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+#from PIL import Image, ImageTk 
+# (cuando instalen la libreria pillow saquenle el "#")
+import requests
+
+url:str = "http://vps-3701198-x.dattaweb.com:4000/"
+def main
